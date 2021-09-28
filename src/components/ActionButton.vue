513cda9bd20de89aa93dd7c02b6fd185d88fe85c
Jesus6Connex <jesus.velazquez@6connex.com>
2021-09-28 18:59:52 -0500

    update classes

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,6 +1,6 @@
 <template>
   <b-button
-    class="tw-bg-pink-800 hover:tw-bg-pink-700 hover:tw-scale-105 tw-w-full tw-text-xl md:tw-text-2xl tw-px-6 tw-py-4 tw-border-none tw-shadow"
+    class="tw-bg-pink-800 hover:tw-bg-pink-700 hover:tw-scale-105 tw-w-full tw-text-xl md:tw-text-2xl tw-px-6 tw-py-4 tw-border-none tw-shadow-lg tw-rounded"
     :class="classes"
     @click="$emit('click')"
   >
