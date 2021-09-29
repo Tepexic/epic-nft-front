62c96a148c204cb8247ab9beb3a21452116e8489
Jesus6Connex <jesus.velazquez@6connex.com>
2021-09-28 19:01:32 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,53 @@
     >
       <the-header />
       <main
-        class="p-4 tw-flex tw-flex-col tw-items-center tw-text-xl md:tw-text-2xl tw-w-full md:tw-w-4/5 lg:tw-w-3/5 xl:tw-w-1/2 tw-mx-auto"
+        class="p-4 tw-flex tw-flex-col tw-items-center tw-text-xl tw-w-full md:tw-w-4/5 lg:tw-w-3/5 xl:tw-w-1/2 tw-mx-auto"
       >
+        <!-- <img
+          v-if="newNFT.owner"
+          :src="newNFT.opensea.image_url"
+          :alt="newNFT.opensea.name"
+          class="tw-rounded tw-shadow"
+        /> -->
         <img
+          v-if="!newNFT.owner"
           src="@/assets/nft_example.svg"
           alt="NFT Example"
           class="tw-rounded tw-shadow"
         />
-        <h2 class="tw-my-10">
+        <h2 class="tw-my-8 tw-p-2" v-if="!newNFT.owner">
           Acuña un NFT único (en formato SVG) en la red Rinkeby con la
           descripción que tendrías si fueras artista.
         </h2>
+        <div
+          class="tw-p-2 tw-w-full tw-text-center tw-bg-gradient-to-r tw-from-pink-800 tw-to-blue-800 tw-my-8 tw-rounded"
+          v-else
+        >
+          <p>
+            ¡Tu nuevo NFT
+            <span class="tw-font-bold tw-text-pink-200">
+              #{{ newNFT.tokenId }}</span
+            >
+            ha sido acuñado y enviado a tu monedero!
+          </p>
+          <p class="tw-mt-2">
+            Encuentralo en
+            <a
+              :href="
+                `https://testnets.opensea.io/assets/${contractAddress}/${newNFT.tokenId}`
+              "
+              target="_blank"
+              class="tw-font-medium hover:tw-bg-white hover:tw-text-pink-800 hpver:tw-rounded"
+              >OpenSea dando click aquí.
+            </a>
+            Podría tomar hasta 10 minutos para que aparezca.
+          </p>
+        </div>
+        <!-- <div
+          class="tw-p-2 tw-w-full tw-text-center tw-bg-gradient-to-r tw-from-pink-800 tw-to-blue-800 tw-mb-8 tw-rounded"
+        >
+          Tokens acuñados: {{ tokenCount }}/50
+        </div> -->
         <action-button
           text="Conecta tu monedero"
           v-if="!account"
@@ -39,7 +75,7 @@
               d="M14.79,10.62L3.5,21.9L2.1,20.5L13.38,9.21L14.79,10.62M19.27,7.73L19.86,7.14L19.07,6.35L19.71,5.71L18.29,4.29L17.65,4.93L16.86,4.14L16.27,4.73C14.53,3.31 12.57,2.17 10.47,1.37L9.64,3.16C11.39,4.08 13,5.19 14.5,6.5L14,7L17,10L17.5,9.5C18.81,11 19.92,12.61 20.84,14.36L22.63,13.53C21.83,11.43 20.69,9.47 19.27,7.73Z"
             />
           </svg>
-          <div>Minando</div>
+          <div>Minando, espera por favor...</div>
         </div>
       </main>
       <the-footer />
@@ -66,9 +102,15 @@ export default {
       provider: null,
       signer: null,
       contract: null,
-      contractAddress: "0x2911Ad7Dc1469043D35217ab6E5f8B3466b85E2f",
+      contractAddress: "0xc1AcfB323D4427F7025Cdef414037D23B9574edb",
       mining: false,
       txn: null,
+      tokenCount: 0,
+      newNFT: {
+        tokenId: 0,
+        owner: null,
+        opensea: null,
+      },
     };
   },
   methods: {
@@ -106,12 +148,21 @@ export default {
           method: "eth_requestAccounts",
         });
         if (this.accounts.length) {
-          this.account = this.accounts[0];
-          console.log(`Found an authorized account: ${this.account}`);
-          if (!this.contract) {
-            this.connectContract();
+          if (window.ethereum.networkVersion === "4") {
+            // rinkeby
+            this.account = this.accounts[0];
+            console.log(`Found an authorized account: ${this.account}`);
+            if (!this.contract) {
+              this.connectContract();
+            }
+            this.makeToast("Wallet found", `Wallet connected!`, "success");
+          } else {
+            this.makeToast(
+              "Wrong Network",
+              `Error: Please make sure your're using the Rinkeby Network`,
+              "danger"
+            );
           }
-          this.makeToast("Wallet found", `Wallet connected!`, "success");
         } else {
           console.log("No accounts found");
           this.makeToast(
@@ -134,9 +185,14 @@ export default {
             MyEpicNFT.abi,
             this.signer
           );
+          this.contract.on("NewEpicNFTMinted", this.newNFTMinted);
         }
       } catch (e) {
-        this.makeToast("Error connecting to contract", `Error: ${e}`, "danger");
+        this.makeToast(
+          "Error connecting to contract",
+          `Error: ${e.message}`,
+          "danger"
+        );
         console.error(e);
       }
     },
@@ -144,6 +200,11 @@ export default {
       this.makeToast("No Wallet detected", "Please get metamask", "danger");
     },
     async mintNFT() {
+      this.newNFT = {
+        tokenId: 0,
+        owner: null,
+        opensea: null,
+      };
       try {
         this.txn = await this.contract.makeAnEpicNFT();
         this.mining = true;
@@ -157,12 +218,46 @@ export default {
         console.log(
           `Mined, see transaction: https://rinkeby.etherscan.io/tx/${this.txn.hash}`
         );
+        //this.getNFTCount();
       } catch (e) {
-        this.makeToast("Error minting NFT", `Error: ${e}`, "danger");
+        this.makeToast("Error minting NFT", `Error: ${e.message}`, "danger");
         console.error(e);
         this.mining = false;
       }
     },
+    async getNFTCount() {
+      // not used, since it requires gas fees
+      try {
+        const count = await this.contract.getNFTCount();
+        this.tokenCount = count.toString();
+      } catch (e) {
+        console.error(e);
+        this.makeToast("Error minting NFT", `Error: ${e.message}`, "danger");
+      }
+    },
+    async newNFTMinted(from, tokenId) {
+      if (from) {
+        this.newNFT.tokenId = tokenId.toNumber();
+        this.newNFT.owner = from;
+        this.makeToast("NFT Minted!", `NFT number ${tokenId + 1}`, "success");
+        console.log(
+          `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${
+            this.contractAddress
+          }/${tokenId.toNumber()}`
+        );
+        // const options = { method: "GET" };
+        // await fetch(
+        //   `https://rinkeby-api.opensea.io/api/v1/asset/${this.contractAddress}/${tokenId}/`,
+        //   options
+        // )
+        //   .then((response) => response.json())
+        //   .then((response) => {
+        //     this.newNFT.opensea = { ...response };
+        //   })
+        //   .catch((err) => console.error(err));
+      }
+    },
+    getNFTfromOS() {},
   },
   async created() {
     document.title = "Ethereum NFT Collection";
